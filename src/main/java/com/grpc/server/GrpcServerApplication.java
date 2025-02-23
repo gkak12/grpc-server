20ceadbb144cdf214983e12918fd6a514f915df9
hyunjun <hyunjun5681@kakao.com>
2025-02-23 22:27:32 +0900

    메인 어플리케이션 수정

--- src/main/java/com/grpc/server/GrpcServerApplication.java
+++ src/main/java/com/grpc/server/GrpcServerApplication.java
@@ -1,7 +1,13 @@
 package com.grpc.server;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = "com.grpc.server")
 public class GrpcServerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(GrpcServerApplication.class, args);
     }
 }
